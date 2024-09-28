9047de604aa5788a4b21b589b677a5ca57598285
Syarif-Ys <5yarif.y.s@gmail.com>
2024-09-28 12:07:54 +0800

    12:07

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,7 +1,11 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
 const AddTask = () => {
   return (
     <div>
-        <button className="btn btn-primary w-full">Add New Task</button>
+        <button className="btn btn-primary w-full">Add New Task
+        <AiOutlinePlus className="ml-2" size={18} />
+        </button>
     </div>
   )
 }
